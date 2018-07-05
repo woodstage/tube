a0e49249bb3c72f1d713693242bf3bc0d002496c
wangpin34 <270280522@qq.com>
2018-07-05 10:56:35 +0800

    Change to context menu

--- background.js
+++ background.js
@@ -1,10 +1,16 @@
-chrome.browserAction.onClicked.addListener(function(){
-	//发送消息到content script
-		chrome.tabs.getSelected(null,function(tab){
-			//tab是用户当前停留的标签
-			//下面的脚本发送消息到指定标签的content script
-			chrome.tabs.sendMessage(tab.id, {cmd: 'init'}, function(response) {
-		    	console.log(response && response.status);
-		  	});
-		})
-})
+// chrome.browserAction.onClicked.addListener(function(){
+// 	//发送消息到content script
+// 		chrome.tabs.getSelected(null,function(tab){
+// 			//tab是用户当前停留的标签
+// 			//下面的脚本发送消息到指定标签的content script
+// 			chrome.tabs.sendMessage(tab.id, {cmd: 'init'}, function(response) {
+// 		    	console.log(response && response.status);
+// 		  	});
+// 		})
+// })
+
+chrome.contextMenus.create({
+	title: "Tube",
+	contexts: ['image'],
+	onclick: function(){debugger;alert('您点击了右键菜单！');}
+});
--- toolbox/index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-	document.getElementsByClassName('close')[0].addEventListener('click', function(){
-		window.open('','_self');
-		window.close();
-	})
-	
-})
